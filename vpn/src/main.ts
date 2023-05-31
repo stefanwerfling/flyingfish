7173c6575682a98a15e2759ded5e16a46af1a461
Stefan Werfling <stefan.werfling@pegenau.de>
2023-05-31 17:04:34 +0200

    fix vpn tuntap2 for ESM

--- vpn/src/main.ts
+++ vpn/src/main.ts
@@ -1,23 +1,18 @@
 import * as process from 'process';
-import {Tun} from 'tuntap2/dist/index.js';
+import {Tun} from 'tuntap2';
 import {IP} from './inc/Decode/IP.js';
 import {IPv4} from './inc/Decode/IPv4.js';
-import {VpnServer} from './inc/VpnServer.js';
 
 /**
  * Main
  */
 (async(): Promise<void> => {
     try {
-        const server = new VpnServer({});
-        server.listen();
-
         const tun = new Tun();
 
         tun.mtu = 1400;
         tun.ipv4 = '4.3.2.1/24';
         tun.on('data', (buf) => {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const ippacket = IP.decodeIP(buf);
             console.log(ippacket);
 
@@ -26,6 +21,7 @@ import {VpnServer} from './inc/VpnServer.js';
                 console.log(ippacket.getDestinationAddress().toString());
             }
         });
+
         tun.isUp = true;
         // tun.release();
 
