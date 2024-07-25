cf35d35b460ddb2dc36d3b9514e1a0543ad0dbff
Stefan Werfling <stefan.werfling@googlemail.com>
2024-07-25 09:59:50 +0200

    update vpn build, add node-rohc

--- vpn/src/main.ts
+++ vpn/src/main.ts
@@ -2,6 +2,7 @@ import * as process from 'process';
 import {Tun} from 'tuntap2';
 import {IP} from './inc/Decode/IP.js';
 import {IPv4} from './inc/Decode/IPv4.js';
+import {Rohc} from 'node-rohc';
 
 /**
  * Main
@@ -9,12 +10,15 @@ import {IPv4} from './inc/Decode/IPv4.js';
 (async(): Promise<void> => {
     try {
         const tun = new Tun();
+        const rohc = new Rohc();
 
         tun.mtu = 1400;
         tun.ipv4 = '4.3.2.1/24';
         tun.on('data', (buf) => {
+            const cmpBuf = rohc.compress(new Uint8Array(buf));
             const ippacket = IP.decodeIP(buf);
             console.log(ippacket);
+            console.log(cmpBuf);
 
             if (ippacket instanceof IPv4) {
                 console.log(ippacket.getSourceAddress().toString());
